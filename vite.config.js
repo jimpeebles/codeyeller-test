d90f5f62a5e579b2742f402c828074705ba13ff8
jimpeebles <james@concepttoproduct.us>
2024-06-20 00:35:26 -0400

    stuf

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/codeyeller-test/',
+  // base: '/codeyeller-test/',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
 })
